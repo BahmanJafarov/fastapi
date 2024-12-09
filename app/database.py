38cb2a17d1d09893afcc38b6ce6c1e089470d766
BahmanJafarov <jrbahmanjafarov@gmail.com>
2024-12-09 14:05:16 +0400

    minor changes

--- app/database.py
+++ app/database.py
@@ -7,7 +7,11 @@ import psycopg2
 import time
 
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
+SQLALCHEMY_DATABASE_URL = (
+    f"postgresql://{settings.database_username}:"
+    f"{settings.database_password}@{settings.database_hostname}:"
+    f"{settings.database_port}/{settings.database_name}"
+)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
