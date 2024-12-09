c359543de8ad2b855215075b189b4618de98409f
BahmanJafarov <jrbahmanjafarov@gmail.com>
2024-12-09 12:28:32 +0400

    env variables

--- app/database.py
+++ app/database.py
@@ -1,8 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from .config import settings
+from psycopg2.extras import RealDictCursor
+import psycopg2
+import time
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:1997138@localhost/fastapi'
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
@@ -16,3 +21,16 @@ def get_db():
         yield db
     finally:
         db.close()
+
+# while True:
+#     try:
+#         conn = psycopg2.connect(host="localhost", database="fastapi", 
+#                                 user="postgres", password="1997138",
+#                                 cursor_factory=RealDictCursor)
+#         cursor = conn.cursor()
+#         print("Database connection was succesfull!")
+#         break
+#     except Exception as error:
+#         print("Connection to database failed")
+#         print("Error: ", error)
+#         time.sleep(2)
--- app/main.py
+++ app/main.py
@@ -1,29 +1,15 @@
 from fastapi import FastAPI
-import psycopg2
-from psycopg2.extras import RealDictCursor
-import time
 from . import models
 from .database import engine
 from .routers import post, user, auth
+from .config import settings
+
+print(settings.model_dump())
 
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-while True:
-    try:
-        conn = psycopg2.connect(host="localhost", database="fastapi", 
-                                user="postgres", password="1997138",
-                                cursor_factory=RealDictCursor)
-        cursor = conn.cursor()
-        print("Database connection was succesfull!")
-        break
-    except Exception as error:
-        print("Connection to database failed")
-        print("Error: ", error)
-        time.sleep(2)
-        
 
 app.include_router(post.router)
 app.include_router(user.router)
